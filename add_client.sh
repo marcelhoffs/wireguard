40b2a7eaa1cd505c5df1da03fca3893e281f9fa2
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-14 16:05:30 +0200

    formatted

--- add_client.sh
+++ add_client.sh
@@ -11,121 +11,114 @@ CLIENT_IP=''
 # Functions
 # ---------------------------------
 
-init()
-{
+init() {
     # Clear the screen
     clear
 }
 
 # ---------------------------------
 
-setup_questions()
-{
-  echo ''  
-  echo -e ' ╔════════════════════════════════════════╗'
-  echo -e ' ║ WireGuard server configurator          ║'
-  echo -e ' ║ Marcel Hoffs, 13.04.2021               ║'
-  echo -e ' ║ Version 1.0                            ║'
-  echo -e ' ╚════════════════════════════════════════╝'
-  echo ''
+setup_questions() {
+    echo ''
+    echo -e ' ╔════════════════════════════════════════╗'
+    echo -e ' ║ WireGuard server configurator          ║'
+    echo -e ' ║ Marcel Hoffs, 13.04.2021               ║'
+    echo -e ' ║ Version 1.0                            ║'
+    echo -e ' ╚════════════════════════════════════════╝'
+    echo ''
 
-  # Client name
-  while [ "$CLIENT_NAME" == '' ]; do
-    read -r -p ' 1)  Client name [e.g. THINKPAD] : ' CLIENT_NAME
-    CLIENT_NAME=${CLIENT_NAME,,}
-  done
+    # Client name
+    while [ "$CLIENT_NAME" == '' ]; do
+        read -r -p ' 1)  Client name [e.g. THINKPAD] : ' CLIENT_NAME
+        CLIENT_NAME=${CLIENT_NAME,,}
+    done
 
-  # Client IP address
-  while [ "$CLIENT_IP" == '' ]; do
-    read -r -p ' 2)  Client IP address : ' CLIENT_IP
-    CLIENT_IP=${CLIENT_IP,,}
-  done
+    # Client IP address
+    while [ "$CLIENT_IP" == '' ]; do
+        read -r -p ' 2)  Client IP address : ' CLIENT_IP
+        CLIENT_IP=${CLIENT_IP,,}
+    done
 
-  echo ''
+    echo ''
 }
 
 # ---------------------------------
 
-generate_client_keys()
-{
-  NAME=$1
+generate_client_keys() {
+    NAME=$1
 
-  # Generate the keypair
-  ./library/gen_keypair.sh "$NAME"
-  mv "$NAME"_privatekey keys
-  mv "$NAME"_publickey keys  
+    # Generate the keypair
+    ./library/gen_keypair.sh "$NAME"
+    mv "$NAME"_privatekey keys
+    mv "$NAME"_publickey keys
 }
 
 # ---------------------------------
 
-get_data()
-{
-  NAME=$1
+get_data() {
+    NAME=$1
 
-  # Get the server public key from the file
-  SERVER_PUBLIC_KEY=$(<keys/SERVER_publickey)
+    # Get the server public key from the file
+    SERVER_PUBLIC_KEY=$(<keys/SERVER_publickey)
 
-  # Get the client private key from the file
-  CLIENT_PRIVATE_KEY=$(<keys/"$NAME"_privatekey)
-  CLIENT_PUBLIC_KEY=$(<keys/"$NAME"_publickey)
+    # Get the client private key from the file
+    CLIENT_PRIVATE_KEY=$(<keys/"$NAME"_privatekey)
+    CLIENT_PUBLIC_KEY=$(<keys/"$NAME"_publickey)
 
-  # Get server config file
-  SERVER_CONFIG_FILE=$(<"config/server_config_file")
+    # Get server config file
+    SERVER_CONFIG_FILE=$(<"config/server_config_file")
 
-  # Get DNS
-  DNS=$(<"config/dns")
+    # Get DNS
+    DNS=$(<"config/dns")
 
-  # Get End Point
-  END_POINT=$(<"config/endpoint")
+    # Get End Point
+    END_POINT=$(<"config/endpoint")
 }
 
 # ---------------------------------
 
-update_server_config()
-{ 
-  NAME=$1
-  
-  # Update server config file
-  echo " > Updating server configuration file: ""$SERVER_CONFIG_FILE" 
+update_server_config() {
+    NAME=$1
 
-  echo "" >> "$SERVER_CONFIG_FILE"
-  echo "[Peer]" >> "$SERVER_CONFIG_FILE"
-  echo "# ""$NAME" >> "$SERVER_CONFIG_FILE"
-  echo "PublicKey = ""$CLIENT_PUBLIC_KEY" >> "$SERVER_CONFIG_FILE"
-  echo "AllowedIPs = ""$CLIENT_IP""/32" >> "$SERVER_CONFIG_FILE"
+    # Update server config file
+    echo " > Updating server configuration file: ""$SERVER_CONFIG_FILE"
+
+    echo "" >>"$SERVER_CONFIG_FILE"
+    echo "[Peer]" >>"$SERVER_CONFIG_FILE"
+    echo "# ""$NAME" >>"$SERVER_CONFIG_FILE"
+    echo "PublicKey = ""$CLIENT_PUBLIC_KEY" >>"$SERVER_CONFIG_FILE"
+    echo "AllowedIPs = ""$CLIENT_IP""/32" >>"$SERVER_CONFIG_FILE"
 }
 
 # ---------------------------------
 
-generate_client_config()
-{
-  NAME=$1
+generate_client_config() {
+    NAME=$1
 
-  # Generating client config
-  CLIENT_CONFIG_FILE="$NAME"".conf"
-  FULL_CLIENT_CONFIG_FILE="client/""$CLIENT_CONFIG_FILE"
-  echo " > Generating client config file: config/""$CLIENT_CONFIG_FILE" 
-  
-  echo "[Interface]" > "$FULL_CLIENT_CONFIG_FILE"
-  echo "PrivateKey = ""$CLIENT_PRIVATE_KEY" >> "$FULL_CLIENT_CONFIG_FILE"
-  echo "Address = ""$CLIENT_IP""/32" >> "$FULL_CLIENT_CONFIG_FILE"
-  echo "DNS = ""$DNS" >> "$FULL_CLIENT_CONFIG_FILE"
-  echo "" >> "$FULL_CLIENT_CONFIG_FILE"
-  echo "[Peer]" >> "$FULL_CLIENT_CONFIG_FILE"
-  echo "AllowedIPs = 0.0.0.0/0, ::/0" >> "$FULL_CLIENT_CONFIG_FILE"
-  echo "Endpoint = ""$END_POINT" >> "$FULL_CLIENT_CONFIG_FILE"
+    # Generating client config
+    CLIENT_CONFIG_FILE="$NAME"".conf"
+    FULL_CLIENT_CONFIG_FILE="client/""$CLIENT_CONFIG_FILE"
+    echo " > Generating client config file: config/""$CLIENT_CONFIG_FILE"
 
-  chmod 600 "$FULL_CLIENT_CONFIG_FILE"
+    echo "[Interface]" >"$FULL_CLIENT_CONFIG_FILE"
+    echo "PrivateKey = ""$CLIENT_PRIVATE_KEY" >>"$FULL_CLIENT_CONFIG_FILE"
+    echo "Address = ""$CLIENT_IP""/32" >>"$FULL_CLIENT_CONFIG_FILE"
+    echo "DNS = ""$DNS" >>"$FULL_CLIENT_CONFIG_FILE"
+    echo "" >>"$FULL_CLIENT_CONFIG_FILE"
+    echo "[Peer]" >>"$FULL_CLIENT_CONFIG_FILE"
+    echo "AllowedIPs = 0.0.0.0/0, ::/0" >>"$FULL_CLIENT_CONFIG_FILE"
+    echo "Endpoint = ""$END_POINT" >>"$FULL_CLIENT_CONFIG_FILE"
+
+    chmod 600 "$FULL_CLIENT_CONFIG_FILE"
 }
 
 # ---------------------------------
-generate_qr_code()
-{
-  NAME=$1    
+generate_qr_code() {
+    NAME=$1
 
-  # Generate QR Code
-  echo ""
-  ./library/gen_qrcode.sh "$FULL_CLIENT_CONFIG_FILE"
+    # Generate QR Code
+    echo ""
+    ./library/gen_qrcode.sh "$FULL_CLIENT_CONFIG_FILE"
 }
 
 # ---------------------------------
--- setup_server.sh
+++ setup_server.sh
@@ -17,26 +17,24 @@ END_POINT_PORT=''
 # Functions
 # ---------------------------------
 
-init()
-{
-    # Clear the screen
-    clear
+init() {
+  # Clear the screen
+  clear
 
-    # Make library executable
-    chmod -R +x library
-    chmod +x add_client.sh
+  # Make library executable
+  chmod -R +x library
+  chmod +x add_client.sh
 
-    # Create config directory
-    mkdir config
-    mkdir client
-    mkdir keys
+  # Create config directory
+  mkdir config
+  mkdir client
+  mkdir keys
 }
 
 # ---------------------------------
 
-setup_questions()
-{
-  echo ''  
+setup_questions() {
+  echo ''
   echo -e ' ╔════════════════════════════════════════╗'
   echo -e ' ║ WireGuard server configurator          ║'
   echo -e ' ║ Marcel Hoffs, 13.04.2021               ║'
@@ -65,7 +63,7 @@ setup_questions()
     read -r -p ' 4)  What DNS to use [e.g. 1.1.1.1] : ' DNS
   done
 
-   # End Point
+  # End Point
   while [ "$END_POINT" == '' ]; do
     read -r -p ' 5)  End point [e.g. vpn.example.com, an ip address] : ' END_POINT
     END_POINT=${END_POINT,,}
@@ -82,44 +80,42 @@ setup_questions()
 
 # ---------------------------------
 
-store_config()
-{ 
+store_config() {
   # Store config
   echo " > Storing configuration in config directory"
 
   # Store server IP in file
-  echo "$SERVER_IP" > config/server_ip
-  
+  echo "$SERVER_IP" >config/server_ip
+
   # Store server port in file
-  echo "$SERVER_PORT" > config/server_port
-  
+  echo "$SERVER_PORT" >config/server_port
+
   # Store network interface
-  echo "$SERVER_NETWORK" > config/server_network
+  echo "$SERVER_NETWORK" >config/server_network
 
   # Store server config file
-  echo "$SERVER_CONFIG_FILE" > config/server_config_file
+  echo "$SERVER_CONFIG_FILE" >config/server_config_file
 
   # Store DNS settings
-  echo "$DNS" > config/dns
-  
-   # Store End Point
-  echo "$END_POINT"":""$END_POINT_PORT" > config/endpoint
+  echo "$DNS" >config/dns
+
+  # Store End Point
+  echo "$END_POINT"":""$END_POINT_PORT" >config/endpoint
 
   # Set permissions
-  chmod -R 600 config  
+  chmod -R 600 config
   chmod -R 600 keys
 }
 
 # ---------------------------------
 
-generate_server_keys()
-{
+generate_server_keys() {
   NAME=$1
 
   # Generate the keypair
   ./library/gen_keypair.sh "$NAME"
   mv "$NAME"_privatekey keys
-  mv "$NAME"_publickey keys  
+  mv "$NAME"_publickey keys
 
   # Get the private key from the file
   SERVER_PRIVATE_KEY=$(<keys/"$NAME"_privatekey)
@@ -127,17 +123,16 @@ generate_server_keys()
 
 # ---------------------------------
 
-generate_server_config()
-{
+generate_server_config() {
   # Generate the server configuration file
-  echo " > Generating server configuration file: ""$SERVER_CONFIG_FILE" 
+  echo " > Generating server configuration file: ""$SERVER_CONFIG_FILE"
 
-  echo "[Interface]" > "$SERVER_CONFIG_FILE"
-  echo "Address = ""$SERVER_IP" >> "$SERVER_CONFIG_FILE"
-  echo "ListenPort = ""$SERVER_PORT" >> "$SERVER_CONFIG_FILE"
-  echo "PrivateKey = ""$SERVER_PRIVATE_KEY" >> "$SERVER_CONFIG_FILE"
-  echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o ""$SERVER_NETWORK"" -j MASQUERADE" >> "$SERVER_CONFIG_FILE"
-  echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ""$SERVER_NETWORK"" -j MASQUERADE" >> "$SERVER_CONFIG_FILE"
+  echo "[Interface]" >"$SERVER_CONFIG_FILE"
+  echo "Address = ""$SERVER_IP" >>"$SERVER_CONFIG_FILE"
+  echo "ListenPort = ""$SERVER_PORT" >>"$SERVER_CONFIG_FILE"
+  echo "PrivateKey = ""$SERVER_PRIVATE_KEY" >>"$SERVER_CONFIG_FILE"
+  echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o ""$SERVER_NETWORK"" -j MASQUERADE" >>"$SERVER_CONFIG_FILE"
+  echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ""$SERVER_NETWORK"" -j MASQUERADE" >>"$SERVER_CONFIG_FILE"
 
   chown 600 "$SERVER_CONFIG_FILE"
 }
