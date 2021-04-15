a6972d50583978fca9890ea38d3f4b15f1581bc5
Marcel Hoffs <marcelhoffs@gmail.com>
2021-04-15 12:07:07 +0200

    update

--- library/gen_serverconfig.sh
+++ library/gen_serverconfig.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+# ---------------------------------
+# Functions
+# ---------------------------------
+
+generate_config() {
+    cat >"$SERVER_CONFIG_FILE" <<EOL
+    [Interface]
+    Address = ${SERVER_IP}
+    ListenPort = ${SERVER_PORT}
+    PrivateKey = ${SERVER_PRIVATE_KEY}
+    PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o ${SERVER_NETWORK} -j MASQUERADE
+    PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ${SERVER_NETWORK} -j MASQUERADE
+EOL
+}
+
+# ---------------------------------
+# Main
+# ---------------------------------
+
 if [ $# -eq 5 ]; then
     # Arguments
     SERVER_CONFIG_FILE=$5
@@ -9,16 +29,7 @@ if [ $# -eq 5 ]; then
 
     # Generate the server configuration file
     echo " > Generating server configuration file: ""$SERVER_CONFIG_FILE"
-
-    cat > "$SERVER_CONFIG_FILE" << EOF
-    [Interface]
-    Address = $SERVER_IP
-    ListenPort = $SERVER_PORT
-    PrivateKey = $SERVER_PRIVATE_KEY
-    PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $SERVER_NETWORK -j MASQUERADE
-    PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $SERVER_NETWORK -j MASQUERADE
-    EOF
-
+    generate_config
     chown 600 "$SERVER_CONFIG_FILE"
 else
     echo "Provide all arguments"
